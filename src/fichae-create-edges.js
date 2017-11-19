4969e3dc66578857c5ecb8a0002a2162226a8db8
Pedro Boueke <phboueke@gmail.com>
2017-11-19 20:35:42 -0200

    Added edges between yearly city registers

--- src/fichae-create-edges.js
+++ src/fichae-create-edges.js
@@ -8,7 +8,99 @@ var string = require("./utils/string-utils.js");
 var lineByLine = require('n-readlines');
 
 program
-    .option("-t", "--type", "the type of edge to be created")
+    .usage('[options]')
+    .option("-t, --type <tp>", "the type of edge to be created")
+    .option("-s, --source <src>", "the source/collection, if needed")
+    .option("-y, --year <yr>", "the year to start the relations", parseInt)
     .parse(process.argv);
 
-    
+process.on('unhandledRejection', (err, p) => { 
+    console.error(err)
+})
+
+// connect to the database
+const host = process.env.ARANGODB_HOST;
+const port = process.env.ARANGODB_PORT;
+const database = process.env.ARANGODB_DB;
+const username = process.env.ARANGODB_USERNAME;
+const password = process.env.ARANGODB_PASSWORD;
+const db = new arangojs.Database({
+    url: `http://${username}:${password}@${host}:${port}`,
+    databaseName: database
+});
+
+var debug = {
+    type: "years",
+    source: "firjan-geral",
+    year: "2006"
+}
+
+var yr = program.year || debug.year;
+var tp = program.type || debug.type;
+var src = program.src || debug.source;
+
+// adds relationships between yearly the objects from the same city 
+if (tp === "years")  { 
+    console.log(`[${tp}] Starting from ${yr} for source ${src}`);
+    (async () => {
+        let counter = 0;
+        let edge_counter = 0;
+        var col = db.collection(src);
+        const q = arangojs.aql`
+            FOR city IN ${col}
+            FILTER city.year == ${yr.toString()}
+            RETURN city
+            `;
+        console.log(q);
+        const cur = await db.query(q)
+
+        processItem(cur.next());
+
+        async function addYearEdge (city) {
+            let sq = arangojs.aql`
+                FOR city IN ${col}
+                FILTER city.location == ${city.location} 
+                AND city.year == ${(parseInt(city.year) + 1).toString()}
+                RETURN city
+            `; 
+
+            let abort = false;
+            try {
+                var future_city = await db.collection(src).document(city.location+"_"+(parseInt(city.year)+1).toString());
+            } catch (err) {
+                abort = true;
+            }
+
+            if (abort || !future_city || future_city === null) {
+                counter += 1;
+                console.log(`[${counter} / ${edge_counter}] Done with city: ${city.location}`);
+                return;
+            }
+
+            let edge = {
+                _from: city._id,
+                _to: future_city._id,
+                location: city.location,
+                delta: (parseFloat(future_city.score)-parseFloat(city.score))
+            }
+
+            await db.collection(src+"-edges").save(edge);
+            edge_counter += 1;
+
+            Promise.resolve().then(() => addYearEdge(future_city));
+        }
+
+        async function processItem(item) {
+            if (await item === null || !item) {
+                console.log("All Done, total inserted: " + counter); 
+                process.exit();
+            }
+            await addYearEdge(await item);
+            counter += 1;
+
+            Promise.resolve().then(() => processItem( cur.next()));
+        }
+    })();
+} else {
+    throw (tp + " is not a supported type");
+}
--- src/fichae.js
+++ src/fichae.js
@@ -5,4 +5,5 @@ const program = require('commander');
 program
   .version('0.0.1')
   .command('load <source> <file>','loads a data file into the database')
+  .command('create-edges','creates the edge objects of specific pre defined types')
   .parse(process.argv); // end with parse to parse through the input
